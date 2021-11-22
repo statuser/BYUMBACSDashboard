6e3c50a5b6f12717a5aa9020b4142cbba56de287
John Howell <john_howell@byu.edu>
2021-11-22 15:23:23 -0700

    Add in HTML student page

--- app/routes.py
+++ app/routes.py
@@ -6,10 +6,22 @@ from app import app
 def index():
     return render_template("index.jinja", title="Main")
 
-@app.route('/student')
-@app.route('/student/<name>')
-def student(name=None):
-    return render_template("student.jinja", title="Student Page", name=name)
+@app.route("/student")
+@app.route('/student/<studentID>')
+def student(studentId = 0):
+    # Load the student data from the database
+    # student = load student from database
+    student = {
+        'firstName' : 'Sophie',
+        'lastName' : 'Rose',
+        'class' : '2022',
+        'track' : 'Marketing',
+        'mentor' : 'Jon Kent',
+        'interest' : 'Tech',
+        'profileImage' : 'https://source.unsplash.com/Av_NirIguEc/600x600'
+    }
+    # Send the data to webpage to render
+    return render_template("student.jinja", title="Student Page", student=student)
 
 @app.route('/career')
 def career():
