eea0b5574965cb0ace59daa08caa77a36d495e85
John Howell <john.r.howell@gmail.com>
2021-11-26 14:00:48 -0700

    Implement First draft of database models

--- app/models.py
+++ app/models.py
@@ -8,6 +8,7 @@ class User(UserMixin, db.Model):
     username = db.Column(db.String(64), index=True, unique=True, nullable=False)
     password_hash = db.Column(db.String(128))
     role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
+    student = db.relationship('Student', backref='user', lazy=True)
     
     def set_password(self, password):
         self.password_hash = generate_password_hash(password)
@@ -16,7 +17,7 @@ class User(UserMixin, db.Model):
         return check_password_hash(self.password_hash, password)
     
     def __repr__(self) -> str:
-        return '<User {}>'.format(self.username)
+        return f'<User {self.username}>'
     
 class Roles(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -26,3 +27,171 @@ class Roles(db.Model):
 @login.user_loader
 def load_user(id):
     return User.query.get(int(id))
+
+class Student(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    firstName = db.Column(db.String(128))
+    middleName = db.Column(db.String(128))
+    lastName = db.Column(db.String(128))
+    studentEmail = db.Column(db.String(128))
+    phone = db.Column(db.String(16))
+    classYear = db.Column(db.String(4))
+    linkedIn = db.Column(db.String(128))
+    intent = db.Column(db.String(128))
+    attitude = db.Column(db.String(128))
+    photo = db.Column(db.LargeBinary)
+    lastUpdate = db.Column(db.DateTime)
+    jobStatus = db.Column(db.String(64))
+    createdDate = db.Column(db.DateTime)
+    userID = db.Column(db.Integer, db.ForeignKey('user.id'))
+    lampList = db.relationship('LampList', backref='student', lazy=True)
+    resume = db.relationship('Resume', backref='student', lazy=True)
+    education = db.relationship('Education', backref='student', lazy = True)
+    infoInterviews = db.relationship('InfoInterview', backref='student', lazy = True)
+    realInterviews = db.relationship('RealInterview', backref='student', lazy = True)
+    mockInterviews = db.relationship('MockInterview', backref='student', lazy = True)
+    jobApplications = db.relationship('JobApplication', backref='student', lazy = True)
+    jobOffers = db.relationship('JobOffer', backref='student', lazy = True)
+    notes = db.relationship('StudentNote', backref='student', lazy=True)
+    
+    
+    
+    def __repr__(self) -> str:
+        return f'<Student: {self.firstName} {self.middleName} {self.lastName}, Class of {self.classYear}>'
+    
+class LampList(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    companyId = db.Column(db.Integer, db.ForeignKey('company.id'))
+    studentId = db.Column(db.Integer, db.ForeignKey('student.id'))
+    date = db.Column(db.DateTime)
+   
+    
+class Resume(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    studentId = db.Column(db.Integer, db.ForeignKey('student.id'))
+    companyId = db.Column(db.Integer, db.ForeignKey('company.id'))
+    startDate = db.Column(db.DateTime)
+    endDate = db.Column(db.DateTime)
+    jobTitle = db.Column(db.String(128))
+    compensation = db.Column(db.String(32))
+    duties = db.Column(db.Text)
+    timestamp = db.Column(db.DateTime)
+    
+class Education(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    studentId = db.Column(db.Integer, db.ForeignKey('student.id'))
+    school = db.Column(db.String(128))
+    startDate = db.Column(db.DateTime)
+    endDate = db.Column(db.DateTime)
+    degree = db.Column(db.String(64))
+    program = db.Column(db.String(64))
+    gpa = db.Column(db.Float)
+    timestamp = db.Column(db.DateTime)
+    
+class StudentNote(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    studentId = db.Column(db.Integer, db.ForeignKey('student.id'), nullable=False)
+    author = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    noteType = db.Column(db.String(64))
+    note = db.Column(db.Text)
+    timestamp = db.Column(db.DateTime)
+    
+class Company(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(128))
+    address1 = db.Column(db.String(128))
+    address2 = db.Column(db.String(128))
+    city = db.Column(db.String(64))
+    state = db.Column(db.String(64))
+    zipCode = db.Column(db.String(64))
+    country = db.Column(db.String(64))
+    numberEmployees = db.Column(db.Integer)
+    relationship = db.Column(db.String(128))
+    notes = db.relationship('CompanyNotes', backref='company', lazy=True)
+    infoInterviews = db.relationship('InfoInterview', backref='company', lazy = True)
+    realInterviews = db.relationship('RealInterview', backref='company', lazy = True)
+    mockInterviews = db.relationship('MockInterview', backref='company', lazy = True)
+    jobApplications = db.relationship('JobApplication', backref='company', lazy = True)
+    jobOffers = db.relationship('JobOffer', backref='company', lazy = True)
+    notes = db.relationship('CompanyNote', backref='student', lazy=True)
+    
+    def __repr__(self) -> str:
+        return f'<Company: {self.name}>'
+    
+    
+class CompanyNote(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    companyId = db.Column(db.Integer, db.ForeignKey('company.id'), nullable=False)
+    author = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    noteType = db.Column(db.String(64))
+    note = db.Column(db.Text)
+
+    
+class Advocate(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    companyId = db.Column(db.Integer, db.ForeignKey('company.id'), nullable=False)
+    firstName = db.Column(db.String(128))
+    lastName = db.Column(db.String(128))
+    email = db.Column(db.String(128))
+    linkedin = db.Column(db.String(128))
+    phone = db.Column(db.String(16))
+    numberHelped = db.Column(db.Integer)
+    companyStart = db.Column(db.DateTime)
+    status = db.Column(db.String(128))
+
+
+class InfoInterview(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    studentId = db.Column(db.Integer, db.ForeignKey('student.id'), nullable=False)
+    companyId = db.Column(db.Integer, db.ForeignKey('company.id'))
+    advocateId = db.Column(db.Integer, db.ForeignKey('advocate.id'))
+    date = db.Column(db.DateTime)
+    helpful = db.Column(db.String(10)) # Values should be one of: Advocate, Support, Nuteral, Detractor
+    gaveRef = db.Column(db.Boolean)
+    note = db.Column(db.Text)
+    
+class RealInterview(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    studentId = db.Column(db.Integer, db.ForeignKey('student.id'), nullable=False)
+    companyId = db.Column(db.Integer, db.ForeignKey('company.id'), nullable=False)
+    date = db.Column(db.DateTime)
+    round = db.Column(db.String(10)) # Values should be one of: First, Second, Final, Other
+    offerConfidence = db.Column(db.Integer) # Values should be 1-6
+    position = db.Column(db.String(128))
+    note = db.Column(db.Text)
+    
+
+class MockInterview(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    studentId = db.Column(db.Integer, db.ForeignKey('student.id'), nullable=False)
+    companyId = db.Column(db.Integer, db.ForeignKey('company.id'))
+    date = db.Column(db.DateTime)
+    position = db.Column(db.String(128))
+    interviewerName = db.Column(db.String(128))
+    note = db.Column(db.Text)
+    
+
+class JobApplication(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    studentId = db.Column(db.Integer, db.ForeignKey('student.id'), nullable=False)
+    companyId = db.Column(db.Integer, db.ForeignKey('company.id'), nullable=False)
+    date = db.Column(db.DateTime)
+    position = db.Column(db.String(128))
+    interestLevel = db.Column(db.Integer) # Values should be 1-6
+    confidenceLevel = db.Column(db.Integer) # Values should be 1-6
+    
+
+class JobOffer(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    studentId = db.Column(db.Integer, db.ForeignKey('student.id'), nullable=False)
+    companyId = db.Column(db.Integer, db.ForeignKey('company.id'), nullable=False)
+    date = db.Column(db.DateTime)
+    position = db.Column(db.String(128))
+    interestLevel = db.Column(db.Integer) # Values should be 1-6
+    compensation = db.Column(db.Float)
+    bonus = db.Column(db.Float)
+    movingExpenses = db.Column(db.Boolean) # Maybe should be a float?
+    stockOptions = db.Column(db.Boolean)
+    totalPackage = db.Column(db.Float)
+    intention = db.Column(db.String(32)) # Values should be one of: Will accept, Need to negotiate, Not Interested, Waiting to compare
+    
